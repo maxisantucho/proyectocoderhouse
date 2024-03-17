fbee16038bb6cd0eea873bc3f8a0cc2df6c1b14e
Maxi Santucho <maximilianosantucho@outlook.es>
2024-03-17 16:44:13 -0300

    1ra entrega

---
+++ proyectocoderhouse/src/main/java/com/proyectocoderhouse/ProyectocoderhouseApplication.java
@@ -0,0 +1,13 @@
+package com.proyectocoderhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectocoderhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectocoderhouseApplication.class, args);
+	}
+
+}
---
+++ proyectocoderhouse/src/test/java/com/proyectocoderhouse/ProyectocoderhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectocoderhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectocoderhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
